c84d2af15ad5a6bbe6bf2e078923f930b1afb141
Julio <julio.olvr@gmail.com>
2020-07-10 19:02:48 -0300

    Moveeeee

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    private Vector2 speed;
+    private float acceleration;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("space")) {
+            this.speed += new Vector2(1.0f, 0.0f);
+        }
 
+        this.transform.localPosition = (Vector2)this.transform.localPosition + this.speed * Time.deltaTime;
     }
 }
