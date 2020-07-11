0032803b47d632a6c368dbfdfa2fcd5edab54c16
Emanuel Kotzayan <emanuelkotzayan@gmail.com>
2020-07-10 21:55:28 -0300

    Basic enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour, IDamageable
+{
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), 0) * Time.deltaTime;
+    }
+
+    public void TakeDamage(float damageTaken)
+    {
+        Destroy(gameObject);
+    }
+}
