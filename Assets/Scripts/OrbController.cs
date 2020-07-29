5fd945cf597044e46d440859a2457fbec95bf154
Emanuel Kotzayan <emanuelkotzayan@gmail.com>
2020-07-29 18:41:55 -0300

    Pilot container and bouncing

--- Assets/Scripts/OrbController.cs
+++ Assets/Scripts/OrbController.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
+using DG.Tweening;
 
-public class OrbController : MonoBehaviour 
+public class OrbController : MonoBehaviour, IDamageable
 {
+    public Sprite PilotSprite;
     public bool IsVisible = false;
 
+    private float Health = 10f;
     private SpriteRenderer SpriteRenderer;
+    private CircleCollider2D Collider;
+
     private Camera Camera;
     private float HalfWidth;
 
     void Awake()
     {
         SpriteRenderer = GetComponent<SpriteRenderer>();
+        Collider = GetComponent<CircleCollider2D>();
         Camera = Camera.main;
         HalfWidth = SpriteRenderer.bounds.extents.x;
+    }
+
+    void Start()
+    {
+        InvokeRepeating("Animate", 0, 1.1f);
     }
 
     void Update()
@@ -26,9 +37,49 @@ public class OrbController : MonoBehaviour
         }
     }
 
+    void Animate()
+    {
+        transform
+				.DOMoveY(transform.position.y + 0.5f, 0.5f)
+				.SetEase(Ease.OutQuad)
+				.OnComplete(() =>
+				{	
+					transform
+						.DOMoveY(transform.position.y - 0.5f, 0.5f)
+						.SetEase(Ease.InQuad);
+				});
+    }
+
+    public bool CanBeConsumed()
+    {
+        return (Health <= 0);
+    }
+
     public void Consume()
     {
-        //AudioSource.PlayClipAtPoint(Sound, transform.position);
-        Destroy(gameObject);
+        if(CanBeConsumed()) {
+            //AudioSource.PlayClipAtPoint(Sound, transform.position);
+            Destroy(gameObject);
+        }
+    }
+
+    public bool IsEnemy()
+    {
+        return false;
+    }
+
+    public bool TakeDamage(float damageTaken) {
+        Health -= damageTaken;
+
+        if(Health <= 0)
+        {
+            SpriteRenderer.sprite = PilotSprite;
+            Collider.isTrigger = true;
+            return true;
+        } 
+        else 
+        {
+            return false;
+        }
     }
 }
